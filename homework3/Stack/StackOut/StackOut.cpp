eb78c43514f9fb3a2fa8145eea776be3f9481696
Artem Evstafev <vur1artem1cheb@gmail.com>
2022-02-22 19:41:51 +0300

    Did StackOut

---
+++ homework3/Stack/StackOut/StackOut.cpp
@@ -0,0 +1,28 @@
+﻿// StackOut.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void printStack(stack<int>& s2)
+{
+
+    stack<int> s1(s2);
+    for (; !s1.empty(); s1.pop())
+        std::cout << s1.top() << ' ';
+    cout << endl;
+}
+int main()
+{
+    stack<int> s;
+    for (int i = 0; i < 10; i++)
+    {
+        s.push(i);
+    }   
+    cout << "First print: "; printStack(s);
+    cout << "Second print: "; printStack(s);
+    return 0;
+
+}
+
