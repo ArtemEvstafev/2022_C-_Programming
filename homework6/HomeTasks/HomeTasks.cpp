60265a9f6427334de5ebf89d87f9906f2679a133
Artem Evstafev <vur1artem1cheb@gmail.com>
2022-04-22 20:49:03 +0300

    Task 1 is done

---
+++ homework6/HomeTasks/HomeTasks.cpp
@@ -0,0 +1,82 @@
+﻿#include "HomeTasks.h"
+
+using namespace std;
+string del_com(string a) {
+    string res;
+    bool del = false;
+    static bool was = false;
+    static char b = '|';
+
+    for (int i = 0; i < a.size(); i++) {
+        if (i + 1 != a.size()) {
+            if (a[i] == '/' && a[i + 1] == '/' && a.substr(i+2).find(';') == string::npos) {
+                break;
+            }
+                
+        }
+        if(i + 3 < a.size()){
+            if (a[i] == '/' && a[i + 1] == '*' && a[i + 2] == '*' && a[i + 3] == '/')
+                del = true;
+        }
+        if (i + 1 != a.size()) {
+            if (a[i] == '/' && a[i + 1] == '*') {
+                del = true;
+                if ((a.substr(a.find('*') + 1)).find('*') == std::string::npos) {
+                    
+                    was = true;
+                }
+            }
+        }
+        if (i - 1 > 0) {
+            if (a[i - 2] == '*' && a[i - 1] == '/') {
+                del = false;
+                was = false;
+            }
+        }
+        
+        if (b == '/' && a[i] != '*') {
+            
+            was = false;
+        }
+        b = a[i];
+
+        if (del)
+            continue;
+        if (was) {
+           
+            continue;
+        }
+        res.push_back(a[i]);
+    }
+    
+    return res;
+}
+int main()
+{
+    string infilename = "D:\\Repositories\\2022_C++_Programming\\homework6\\HomeTasks\\start.txt";
+    string outfilename = "D:\\Repositories\\2022_C++_Programming\\homework6\\HomeTasks\\finish.txt";
+    vector<string> v;
+    
+    
+    ifstream infile;
+    infile.open(infilename);
+    string str;
+    
+    while (getline(infile, str)) {
+        str = del_com(str);
+        if(str.size() != 0)
+            v.push_back(str);
+        
+    }
+    infile.close();
+
+    
+    ofstream outfile;
+    outfile.open(outfilename);
+    for (auto it = v.begin(); it != v.end(); it++) {
+        outfile << *it<< endl;
+    }
+    outfile.close();
+
+    return 0;
+}
---
+++ homework6/HomeTasks/HomeTasks.h
@@ -0,0 +1,14 @@
+﻿// HomeTasks.h : включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта.
+
+#pragma once
+
+#include <iostream>
+#include <bitset>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+// TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
